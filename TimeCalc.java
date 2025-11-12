ba79e7002e682c46ded9dc99e706cefdac0f7101
LeedorReichman <leedor.yacobi@post.runi.ac.il>
2025-11-12 10:56:08 +0200

    init

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,16 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        // Extract hours and minutes from the input string "HH:MM"
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int addedMinutes = Integer.parseInt(args[1]);
+
+        // Calculate total minutes
+        int totalMinutes = hours * 60 + minutes + addedMinutes;
+
+        int totalHours = (totalMinutes / 60) % 24;
+        int remainingMinutes = totalMinutes % 60;
+
+        System.out.println("" + totalHours + ":" + remainingMinutes);
     }
 }
